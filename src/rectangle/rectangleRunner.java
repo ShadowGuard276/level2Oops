44ff0c6a0787d725155afc5c68f60b2a9e1b262b
avi276271 <63207745+avi276271@users.noreply.github.com>
2020-04-23 01:17:09 -0400

    new rectangle

---
+++ src/rectangle/rectangleRunner.java
@@ -0,0 +1,14 @@
+package rectangle;
+
+public class rectangleRunner {
+
+	public static void main(String[] args) {
+		Rectangle rectangle = new Rectangle(25,30);
+		System.out.println(rectangle);
+		rectangle.setlength(20);
+		System.out.println(rectangle);
+		rectangle.setWidth(25);
+		System.out.println(rectangle);
+	}
+
+}
